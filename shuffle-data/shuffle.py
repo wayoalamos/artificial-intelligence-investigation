97c8d9c2e9356675c07de187ecdd46f3f0a413f7
wayoalamos <malamos2@uc.cl>
2019-06-13 00:52:36 -0400

    making new problems with ex 3

--- shuffle-data/shuffle.py
+++ shuffle-data/shuffle.py
@@ -1,13 +1,8 @@
 import random
 
 def read_lines_from_file():
-    data = []
-    for i in range(643):
-        file_path = "../moves/bin-moves/1.1/sol_ida_problem_"+str(i)+"_.txt"
-        file = open(file_path, "r")
-        data += file.readlines();
-    #print(data)
-    print("length of data: ", len(data))
+    file = open("solutions.txt", "r")
+    data = file.readlines()
     file.close()
 
     random.shuffle(data)
