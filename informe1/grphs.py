25b3568e11eae7051a4040793a9f763cf1e87ca1
wayoalamos <malamos2@uc.cl>
2020-05-29 12:51:14 -0400

    d.

--- informe1/grphs.py
+++ informe1/grphs.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np; np.random.seed(1)
 
-time = [2.8, 102, 42, 54, 122, 46.24, 24.7, 9.25, 8.9, 9.8, 4.13]
-expansions = [2227, 111950, 49069, 56807, 952586, 419630, 213438, 76480, 77848, 90058, 35766]
-length = [267,  259, 279, 261, 235, 235, 239, 251, 263, 287, 335]
-names = ["ANN4", "ANN3", "ANN2", "ANN1", "astar 1", "astar 1.2", "astar 1.3", "astar 1.5", "astar 1.7", "astar 2", "astar 3"]
+time = [25.8, 70.6, 2.8, 122, 46.24, 24.7, 9.25, 8.9, 9.8]
+expansions = [27000, 81067, 2227,  952586, 419630, 213438, 76480, 77848, 90058]
+length = [255, 241,  267,  235, 235, 239, 251, 263, 287]
+names = ["ANN7", "ANN6", "ANN4", "astar 1", "astar 1.2", "astar 1.3", "astar 1.5", "astar 1.7", "astar 2"]
 optimality = [i/235 for i in length]
 
 print(len(time))
 print(len(length))
 print(len(names))
 
-fig, ax = plt.subplots()
-ax.scatter(length, time)
+# fig, ax = plt.subplots()
+# ax.scatter(length, time)
 
-for i, txt in enumerate(names):
-    ax.annotate(txt, (length[i], time[i]))
+# for i, txt in enumerate(names):
+#     ax.annotate(txt, (length[i], time[i]))
 
-plt.xlabel("length")
-plt.ylabel("time (s)")
-plt.show()
+# plt.xlabel("length")
+# plt.ylabel("time (s)")
+# plt.show()
 
 fig, ax = plt.subplots()
 ax.scatter(length, expansions)
